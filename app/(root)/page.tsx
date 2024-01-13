1dea06034c0887f8218df9b9df53ab4d896869d2
wtcrawford <wtcmetcode@gmail.com>
2024-01-12 17:30:11 -0800

    fixing duplicate bottom

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,6 @@ export default async function Home() {
   return (
     <>
       <h1 className='head-text text-left'>Home</h1>
-
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p>No threads found</p>
@@ -35,7 +34,6 @@ export default async function Home() {
           </>
         )}
       </section>
-
     </>
   )
 }
